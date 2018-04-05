af8e2a9d6f94728e0fa98db244f51a8f7d437e42
stevenernst131 <sterns@microsoft.com>
2018-04-05 15:27:15 -0700

    Side Menu Improvements

--- AngularApp/src/app/modules/dashboard/resource-menu-item/resource-menu-item.component.ts
+++ AngularApp/src/app/modules/dashboard/resource-menu-item/resource-menu-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ResourceService } from '../../../shared/services/resource.service';
-import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
+import { ActivatedRoute, Router, NavigationExtras, NavigationEnd } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'resource-menu-item',
@@ -9,9 +10,23 @@ import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 })
 export class ResourceMenuItemComponent implements OnInit {
 
+  selected: boolean = false;
+
+  currentRoutePathSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(null);
+
+  @Input() 
+  set currentRoutePath(path: string[]) {
+    this.currentRoutePathSubject.next(path);
+  }
+
   constructor(public resourceService: ResourceService, private _router: Router, private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.currentRoutePathSubject.subscribe(path => {
+      if(path) {
+        this.selected = path.length === 1 && path[0] === 'home';
+      }
+    });
   }
 
   navigate() {
@@ -22,7 +37,7 @@ export class ResourceMenuItemComponent implements OnInit {
       relativeTo: this._activatedRoute
     };
 
-    this._router.navigate(['./'], navigationExtras);
+    this._router.navigate(['./home'], navigationExtras);
   }
 
 }
