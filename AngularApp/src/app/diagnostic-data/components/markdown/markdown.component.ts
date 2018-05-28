c665bba80e542039a2676a910648ea75ead318db
stevenernst131 <sterns@microsoft.com>
2018-05-28 13:43:43 -0700

    Add copy to markdown

--- AngularApp/src/app/diagnostic-data/components/markdown/markdown.component.ts
+++ AngularApp/src/app/diagnostic-data/components/markdown/markdown.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Rendering, DiagnosticData } from '../../models/detector';
 import { DataRenderBaseComponent } from '../data-render-base/data-render-base.component';
+import { MarkdownService } from 'ngx-markdown';
+import { ClipboardService } from '../../services/clipboard.service';
+import { DIAGNOSTIC_DATA_CONFIG, DiagnosticDataConfig } from '../../config/diagnostic-data-config';
 
 @Component({
   selector: 'markdown-view',
@@ -12,12 +15,18 @@ export class MarkdownComponent extends DataRenderBaseComponent {
   renderingProperties: Rendering;
 
   markdown: string;
+  isPublic: boolean;
+
+  constructor(private _markdownService: MarkdownService, private _clipboard: ClipboardService, @Inject(DIAGNOSTIC_DATA_CONFIG) config: DiagnosticDataConfig) {
+    super();
+    this.isPublic = config && config.isPublic;
+  }
 
   protected processData(data: DiagnosticData) {
     super.processData(data);
     this.renderingProperties = <Rendering>data.renderingProperties;
 
-    this.createViewModel()
+    this.createViewModel();
   }
 
   private createViewModel() {
@@ -25,5 +34,10 @@ export class MarkdownComponent extends DataRenderBaseComponent {
     if(rows.length > 0 && rows[0].length > 0) {
       this.markdown = rows[0][0];
     }
+  }
+
+  copyMarkdown() {
+    let mardownHtml = this._markdownService.compile(this.markdown);
+    this._clipboard.copyAsHtml(mardownHtml);    
   }
 }
