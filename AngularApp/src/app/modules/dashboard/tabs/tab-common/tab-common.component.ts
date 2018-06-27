abf673030a22cc3274154267821794fa4f45133a
stevenernst131 <sterns@microsoft.com>
2018-06-27 09:40:39 -0700

    Fixing scrolling issues

--- AngularApp/src/app/modules/dashboard/tabs/tab-common/tab-common.component.ts
+++ AngularApp/src/app/modules/dashboard/tabs/tab-common/tab-common.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabCommonComponent implements OnInit {
 
+  contentHeight: string;
   constructor() {
+    this.contentHeight = (window.innerHeight - 112) + 'px';
   }
 
   ngOnInit() {
